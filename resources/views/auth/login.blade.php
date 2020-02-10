1b31f8aab52af3120d7161ee910f8ebaacc83996
Temidayojnr <temidayojunior@gmail.com>
2020-02-10 18:18:44 +0100

    Added image for admin login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -4,72 +4,86 @@
 <!------ Include the above in your HEAD tag ---------->
 
 <style>
+    .overlay {
+        
+        background-color: rgba(135,206,235,0.10);
+        background-size: cover;
+    }
     body {
-    margin: 0;
-    padding: 0;
-    background-color: #87ceeb;
-    height: 100vh;
+        margin: 0;
+        padding: 0;
+        /* background: url("../admin/legislature.jpg") no-repeat;  */
+        /* background-color: #87ceeb; */
+        height: 100vh;
+    }
+    .image-container {
+        background: url("../admin/admin.jpg");
+        height: 100vh;
+        background-size: cover; 
     }
     #login .container #login-row #login-column #login-box {
-    margin-top: 120px;
-    max-width: 600px;
-    height: 338px;
-    border: 1px solid #9C9C9C;
-    background-color: #EAEAEA;
+        margin-top: 120px;
+        max-width: 600px;
+        height: 338px;
+        border: 1px solid #9C9C9C;
+        background-color: #EAEAEA;
     }
     #login .container #login-row #login-column #login-box #login-form {
-    padding: 20px;
+        padding: 20px;
     }
     #login .container #login-row #login-column #login-box #login-form #register-link {
-    margin-top: -85px;
+        margin-top: -85px;
     }
 </style>
 
 
 <body>
-    <div id="login">
-        <h3 class="text-center text-white pt-5">Legislators List</h3>
-        <div class="container">
-            <div id="login-row" class="row justify-content-center align-items-center">
-                <div id="login-column" class="col-md-6">
-                    <div id="login-box" class="col-md-12">
-                        <form id="login-form" class="form" action="{{route('login')}}" method="post">
-                            {{ csrf_field() }}
-                            <h3 class="text-center text-info">Login</h3>
-                            <div class="form-group">
-                                <label for="email" class="text-info">{{ __('E-Mail Address') }}</label><br>
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
-
-                                @error('email')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                            <div class="form-group">
-                                <label for="password" class="text-info">{{ __('Password') }}</label><br>
-                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
-
-                                @error('password')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                            <div class="form-group">
-                                <label class="form-check-label" for="remember"><span>{{ __('Remember Me') }}</span> <span><input id="remember" name="remember" type="checkbox" {{ old('remember') ? 'checked' : '' }}></span></label><br>
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Login') }}
-                                </button>
-                            </div>
-                            <div id="register-link" class="text-right">
-                                @if (Route::has('password.request'))
-                                    <a class="btn btn-link" href="{{ route('password.request') }}">
-                                        {{ __('Forgot Your Password?') }}
-                                    </a>
-                                @endif
-                            </div>
-                        </form>
+    <div class="overlay"></div>
+    <div class="image-container">
+        <div id="login">
+            <h3 class="text-center text-white pt-5">Legislators List</h3>
+            <div class="container">
+                <div id="login-row" class="row justify-content-center align-items-center">
+                    <div id="login-column" class="col-md-6">
+                        <div id="login-box" class="col-md-12">
+                            <form id="login-form" class="form" action="{{route('login')}}" method="post">
+                                {{ csrf_field() }}
+                                <h3 class="text-center text-info">Login</h3>
+                                <div class="form-group">
+                                    <label for="email" class="text-info">{{ __('E-Mail Address') }}</label><br>
+                                    <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
+    
+                                    @error('email')
+                                        <span class="invalid-feedback" role="alert">
+                                            <strong>{{ $message }}</strong>
+                                        </span>
+                                    @enderror
+                                </div>
+                                <div class="form-group">
+                                    <label for="password" class="text-info">{{ __('Password') }}</label><br>
+                                    <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
+    
+                                    @error('password')
+                                        <span class="invalid-feedback" role="alert">
+                                            <strong>{{ $message }}</strong>
+                                        </span>
+                                    @enderror
+                                </div>
+                                <div class="form-group">
+                                    <label class="form-check-label" for="remember"><span>{{ __('Remember Me') }}</span> <span><input id="remember" name="remember" type="checkbox" {{ old('remember') ? 'checked' : '' }}></span></label><br>
+                                    <button type="submit" class="btn btn-primary">
+                                        {{ __('Login') }}
+                                    </button>
+                                </div>
+                                <div id="register-link" class="text-right">
+                                    @if (Route::has('password.request'))
+                                        <a class="btn btn-link" href="{{ route('password.request') }}">
+                                            {{ __('Forgot Your Password?') }}
+                                        </a>
+                                    @endif
+                                </div>
+                            </form>
+                        </div>
                     </div>
                 </div>
             </div>
